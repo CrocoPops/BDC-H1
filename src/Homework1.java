4bc91e84388277d5c889d968b758c5ad936b2636
dadeferra2001 <f.davide2001@gmail.com>
2024-03-21 07:53:47 +0100

    Task 1

--- src/Homework1.java
+++ src/Homework1.java
@@ -3,8 +3,6 @@ import java.util.*;
 
 public class Homework1{
 
-    static List<Point> points = new ArrayList<Point>();
-
     public static void main(String[] args) throws IOException {
 
         // Check if the number of arguments is correct
@@ -13,17 +11,64 @@ public class Homework1{
             throw new IllegalArgumentException("Wrong number of params!");
 
         // Read all points in the file and add them to the list of points
-        Scanner scanner = new Scanner(new File(args[0]));
+        Scanner scanner =    new Scanner(new File(args[0]));
+        List<Point> points = new ArrayList<>();
         while (scanner.hasNextLine()) {
             String[] cords = scanner.nextLine().split(",");
             points.add(new Point(Float.parseFloat(cords[0]), Float.parseFloat(cords[1])));
-            System.out.println(cords[0] + ", " + cords[1]);
         }
+
+        ExactOutliers(points, 1f, 2, 15);
 
     }
 
-    public static void ExactOutliers(float D, int M, int K) {
+    public static void ExactOutliers(List<Point> points, float D, int M, int K) {
+        ArrayList<Point> outliers = new ArrayList<>();
 
+        // Calculate the distances
+        // for a total of N(N-1)/2 distances being calculated
+        double[][] distances = new double[points.size()][points.size()];
+        for (int i = 0; i < points.size(); i++)
+            for (int j = i + 1; j < points.size(); j++)
+                distances[i][j] = distances[j][i] = points.get(i).distanceTo(points.get(j));
+
+
+        // Counting close (<= D) points for each point and if they are < M then
+        // the point gets saved into a list as outlier
+        for (int i = 0; i < points.size(); i++) {
+            int count = 0;
+            for (int j = 0; j < points.size(); j++)
+                if (distances[i][j] <= D)
+                    count++;
+
+            if (count < M) {
+                outliers.add(points.get(i));
+            }
+        }
+
+        // Sort outliers based on number of closest points
+        outliers.sort((p1, p2) -> {
+            int count1 = 0, count2 = 0;
+            int index1 = points.indexOf(p1);
+            int index2 = points.indexOf(p2);
+            for (int j = 0; j < points.size(); j++) {
+                if (distances[index1][j] <= D)
+                    count1++;
+
+                if (distances[index2][j] <= D)
+                    count2++;
+
+            }
+            return Integer.compare(count1, count2);
+        });
+
+        // Print the number of (D,M)-outliers
+        System.out.println("Number of (D, M)-outliers: " + outliers.size());
+
+        // Print the first K outliers
+        System.out.println("First " + K + " outliers:");
+        for (int i = 0; i < Math.min(K, outliers.size()); i++)
+            System.out.println("(" + outliers.get(i).getX() + ", " + outliers.get(i).getY() + ")");
     }
 
 }
