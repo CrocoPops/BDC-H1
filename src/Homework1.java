4b39b55aea7d5bcbedf54669deb02fc24fcd9e38
Manuz01 <matteo.manuzzato@gmail.com>
2024-03-21 14:18:41 +0100

    Update Task1

--- src/Homework1.java
+++ src/Homework1.java
@@ -7,10 +7,10 @@ public class Homework1{
 
         // Check if the number of arguments is correct
         System.out.println(args.length);
-        if(args.length != 1)
+        if(args.length != 5)
             throw new IllegalArgumentException("Wrong number of params!");
 
-        // Read all points in the file and add them to the list of points
+        // Read all points in the file and add them to the  (integers), and must compute and print the following information.list of points
         Scanner scanner =    new Scanner(new File(args[0]));
         List<Point> points = new ArrayList<>();
         while (scanner.hasNextLine()) {
@@ -18,7 +18,26 @@ public class Homework1{
             points.add(new Point(Float.parseFloat(cords[0]), Float.parseFloat(cords[1])));
         }
 
-        ExactOutliers(points, 1f, 2, 15);
+        // Read D, M, K parameters
+        float D = Float.parseFloat(args[1]);
+        int M = 1; // Default
+        int K = 1; // Default
+
+        try{
+            M = Integer.parseInt(args[2]);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("Param M is not an integer!");
+        }
+
+        try{
+            K = Integer.parseInt(args[3]);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("Param K is not an integer!");
+        }
+
+        //Add the control for param L in the task2
+
+        ExactOutliers(points, D, M, K);
 
     }
 
@@ -28,9 +47,12 @@ public class Homework1{
         // Calculate the distances
         // for a total of N(N-1)/2 distances being calculated
         double[][] distances = new double[points.size()][points.size()];
-        for (int i = 0; i < points.size(); i++)
-            for (int j = i + 1; j < points.size(); j++)
-                distances[i][j] = distances[j][i] = points.get(i).distanceTo(points.get(j));
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 1; j < points.size(); j++) {
+                distances[i][j] = points.get(i).distanceTo(points.get(j));
+                distances[j][i] = points.get(i).distanceTo(points.get(j));
+            }
+        }
 
 
         // Counting close (<= D) points for each point and if they are < M then
